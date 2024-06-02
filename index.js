2f776e733fe84e5276989b8bb6c0699e42f62392
unknown <sh.rusell@gmail.com>
2024-06-02 17:32:04 +0500

    feat: add new pages

---
+++ index.js
@@ -0,0 +1,99 @@
+$('.major__slider-wrapper').slick({
+  speed: 1000,
+  fade: true,
+  cssEase: 'linear',
+  prevArrow: $('.major__prev'),
+  nextArrow: $('.major__next'),
+});
+
+$('.experts__slider_wrapper').slick({
+  infinite: true,
+  speed: 500,
+  fade: false,
+  cssEase: 'linear',
+  slidesToShow:4,
+  centerMode:false,
+  slidesToScroll:1,
+  variableWidth: true,
+  prevArrow: $('.experts__prev'),
+  nextArrow: $('.experts__next'),
+});
+$('.idea__slider_wrapper').slick({
+  infinite: true,
+  speed: 500,
+  fade: false,
+  cssEase: 'linear',
+  slidesToShow:1,
+  centerMode:false,
+  slidesToScroll:1,
+  variableWidth: true,
+  prevArrow: $('.idea__prev'),
+  nextArrow: $('.idea__next'),
+});
+$('.news__slider_wrapper').slick({
+  infinite: true,
+  speed: 500,
+  fade: false,
+  cssEase: 'linear',
+  slidesToShow:3,
+  centerMode:false,
+  slidesToScroll:1,
+  variableWidth: true,
+  prevArrow: $('.news__prev'),
+  nextArrow: $('.news__next'),
+});
+
+$('.support__stategic__wrapper').slick({
+ infinite: true,
+  speed: 8000,
+  fade: false,
+  cssEase: 'linear',
+  slidesToShow:1,
+  centerMode:false,
+  slidesToScroll:1,
+  variableWidth: true,
+  arrows:false,
+  autoplay: true,
+  autoplaySpeed: 0,
+  responsive: [
+    {
+      breakpoint: 801,
+      settings: { 
+        rows: 2,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+]
+});
+$('.support__info_wrapper').slick({
+  infinite: true,
+  speed: 8000,
+  fade: false,
+  cssEase: 'linear',
+  slidesToShow:1,
+  centerMode:false,
+  slidesToScroll:1,
+  variableWidth: true,
+  arrows:false,
+  autoplay: true,
+  autoplaySpeed: 0,
+  rtl:true,
+  responsive: [
+    {
+      breakpoint: 801,
+      settings: { 
+        rows: 2,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+]
+});
+
+$('.header__burger').click(function(){
+  $(this).toggleClass('active');
+  $('.header__nav').toggleClass('active');
+})
